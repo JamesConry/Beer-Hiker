06db91ac1d29f401533eca2218e95f6251e474b1
James Conry <james.conry@comcast.net>
2019-12-21 08:51:59 -0600

    initial push of boilerplate

---
+++ client/build/precache-manifest.db8f923a691603d2a06b4483fd71cc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebd0b1f3a1a6c7964397",
+    "url": "/static/js/main.82035b6b.chunk.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "ebd0b1f3a1a6c7964397",
+    "url": "/static/css/main.f3c02977.chunk.css"
+  },
+  {
+    "revision": "518c27f59efb428c8a26a27390ad1594",
+    "url": "/index.html"
+  }
+];
