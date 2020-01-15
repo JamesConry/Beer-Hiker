e1bd43a802fe0e9b0b4a4a5bc9f39f3e8ce27ac2
James Conry <james.conry@comcast.net>
2020-01-14 21:34:16 -0600

    map markers workgit add .git add .git add .git add .git add .git add .!

---
+++ client/src/components/map/coord.js
@@ -0,0 +1,34 @@
+const coord = [
+//   {
+//     "id": 1,
+//     "name": "Eric Moore",
+//     "address": "Surulere",
+//     "coordinates": [{"lat": 6.556665, "lng": 3.227095}]
+//   },
+//   {
+//     "id": 2,
+//       "name": "Bariga Square",
+//     "address": "Bariga",
+//     "coordinates": [{"lat": 6.5474387, "lng": 3.3910227}]
+//   },
+//   {
+//     "id": 3,
+//       "name": "Bariga Main Extension",
+//     "address": "Bariga Extension",
+//     "coordinates": [{"lat": 6.6354199, "lng": 3.2996456}]
+//   },
+//   {
+//     "id": 4,
+//       "name": "Printing House",
+//     "address": "Shomolu",
+//     "coordinates": [{"lat": 6.5383092, "lng": 3.3800615}]
+//   },
+//   {
+//     "id": 5,
+//       "name": "Capital of Lagos",
+//     "address": "Ikeja",
+//     "coordinates": [{"lat": 6.6541188, "lng": 3.3064652}]
+//   }
+]
+
+export default coord;
