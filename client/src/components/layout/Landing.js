9b4ac40ea2d5d88766e987346a265bff20c74909
Megan Vogler <mmphilmon@gmail.com>
2020-01-16 21:10:00 -0600

    results and layout css

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,76 +1,58 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import NavBarOut from "../NavBarOut/NavBarOut";
-import "../layout/Landing.css";
+import "./Landing.css";
+import 'bulma/css/bulma.css';
 import logo from '../../images/BeerHikerLogo2.jpg';
+import image from '../../images/beerSamples.jpg';
 
 class Landing extends Component {
   render() {
     return (
-      <div class="background">
-        <div>
-       
-        <section class="hero col s12 center-align">
-          <div class="hero-head">
-            
-            <div class="container">
-            <section class="hero is-large has-sm-img"> 
-          <div class="hero-body"> 
-            <div class="container has-text-centered"> 
-            <div className="container valign-wrapper">
+      <div className="container">
+        <NavBarOut />
+        <br />
+        <div className="start">
+          <div className="columns">
+            <div className="column is-three-fifths">
+              <p>Please Log In or Register For an Account</p>
             </div>
-              </div> 
-            </div> 
-          </section>
+            <div className="column is-one-fifths">
+              <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable orange accent-3"
+              >
+                Register
+                </Link>
             </div>
-          </div>
-        </section>
-        <br/>
-          <div className="row title welcomeContainer">
-            <div className="col s12 center-align">
-              <h4>
-                {" "}
-                <span style={{ fontFamily: "monospace" }}>WELCOME TO</span>
-              </h4>
-              <div class="beerHeader">
-                <img src={logo} className={`logo`} alt='logo'/> 
-              </div>
-              <br/>
-              <p className="flow-text" style={{ fontFamily: "monospace" }}>
-                Please Log In or Register For an Account
-              </p>
-              <br />
-              <div className="col s6">
-                <Link
-                  to="/register"
-                  style={{
-                    width: "140px",
-                    borderRadius: "3px",
-                    letterSpacing: "1px"
-                  }}
-                  className="btn btn-large waves-effect waves-light hoverable orange accent-3"
-                >
-                  Register
+            <div className="column is-one-fifths">
+              <Link
+                to="/login"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable orange accent-3"
+              >
+                Log In
                 </Link>
-              </div>
-              <div className="col s6">
-                <Link
-                  to="/login"
-                  style={{
-                    width: "140px",
-                    borderRadius: "3px",
-                    letterSpacing: "1px"
-                  }}
-                  className="btn btn-large waves-effect waves-light hoverable orange accent-3"
-                >
-                  Log In
-                </Link>
-              </div>
             </div>
           </div>
         </div>
-      
-      
+        <br />
+        <div className="landingBackground">
+          <img src={image} className="landingBackground" alt='beer image' />
+          <figure className="image is-4by3">
+            <iframe className="has-ratio" width="640" height="480" img src="../../images/beerSamples.jpg" frameBorder="0"
+              allowFullScreen></iframe>
+          </figure>
+        </div>
       </div>
     );
   }
