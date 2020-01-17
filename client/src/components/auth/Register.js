57e334f2edaadd264b74ed119d0d2d0f44d4b207
Matt Michael <matthewemichael@gmail.com>
2020-01-17 12:37:27 -0600

    reverting to previous style

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -75,8 +75,12 @@ class Register extends Component {
       </section>
       <br/>
       <div className="container registerForm">
-        <div className="row" style={{ marginTop: "2rem" }}>
+        <div className="row">
           <div className="col s8 offset-s2">
+            <Link to="/" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
               <h4>
                 <b>Register</b> below
