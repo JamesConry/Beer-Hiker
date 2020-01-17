d32cda80d0846571d94cf92dae7682039ed206dc
James Conry <james.conry@comcast.net>
2020-01-16 20:18:02 -0600

    fixed heroku stuff v6

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ const routes = require("./routes");
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 // Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+
+// app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,13 +30,7 @@ app.use(express.json());
 const db = process.env.MONGODB_URI || require("./config/keys").mongoURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true, useCreateIndex: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+
 
 // Passport middleware
 app.use(passport.initialize());
@@ -40,10 +41,15 @@ require("./config/passport")(passport);
 // Routes
 app.use(routes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+mongoose
+  .connect(
+    db
+    // ,
+    // { useNewUrlParser: true, useCreateIndex: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
