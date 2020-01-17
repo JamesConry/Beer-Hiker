18140ca1e18cc4e6d01757c4c1215cb16ab58627
James Conry <james.conry@comcast.net>
2020-01-16 18:49:31 -0600

    fixed heroku stuff v2

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:beerhiker1@ds331568.mlab.com:31568/heroku_35b8ggg4",
+  mongoURI: "mongodb://localhost/mern-auth", //mongodb://admin:beerhiker1@ds331568.mlab.com:31568/heroku_35b8ggg4
   secretOrKey: "secret"
 };
