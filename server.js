de6be2556becfc63c1ebb3366eb896f9d697981e
James Conry <james.conry@comcast.net>
2020-01-16 20:06:14 -0600

    fixed heroku stuff v6

--- server.js
+++ server.js
@@ -41,15 +41,9 @@ require("./config/passport")(passport);
 app.use(routes);
 
 if(process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  const root = require('path').join(__dirname, 'client', 'build')
-  app.use(express.static(root));
-  app.get("*", (req, res) => {
-  res.sendFile('index.html', { root });
-})
+  app.use(express.static("client/build"));
 }
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-
